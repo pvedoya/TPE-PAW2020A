83b5cbdd0878726e3733e776e80190e76484b810
Facundo <fastiz@Araucaria-2.local>
2020-03-24 13:33:28 -0300

    WIP: project configurations

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	
+	public HelloWorldController() {
+		System.out.println("CHAU");
+
+	}
+	
+	@RequestMapping("/")
+	public ModelAndView helloWorld() {
+		final ModelAndView mav = new ModelAndView("index");
+		mav.addObject("greeting", "PAW");
+		return mav;
+	}
+	
+}
